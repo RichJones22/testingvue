0915a13d19d18f17b5b3c74d0231edb2848d0d35
Rich Jones <jones_rich@yahoo.com>
2017-10-24 21:46:30 -0400

    finished lesson 5

--- src/components/CouponCode.vue
+++ src/components/CouponCode.vue
@@ -5,6 +5,9 @@
         <p v-if="valid">
             Coupon Redeemed: {{ message }}
         </p>
+        <p v-else>
+            Invalid Coupon Code
+        </p>
     </div>
 
 </template>
@@ -14,29 +17,39 @@
         data() {
             return {
                 code: '',
+
+                // below should be performed via an axios call; overridden in tests.
                 coupons: [
-                    {
-                        code: '50OFF',
-                        message: '50% Off!',
-                        discount: 50
-                    },
-                    {
-                        code: 'FREE',
-                        message: 'Entirely Free',
-                        discount: 100
-                    }
+//                    {
+//                        code: '50OFF',
+//                        message: '50% Off!',
+//                        discount: 50
+//                    },
+//                    {
+//                        code: 'FREE',
+//                        message: 'Entirely Free',
+//                        discount: 100
+//                    }
                 ],
                 valid: false
             }
         },
         computed: {
+            selectedCoupon() {
+                return this.coupons.find(coupon => coupon.code === this.code);
+            },
             message() {
-                return this.coupons.find(coupon => coupon.code === this.code).message;
+                return this.selectedCoupon.message;
             }
         },
         methods: {
             validate() {
-                this.valid = this.coupons.map(coupon => coupon.code).includes(this.code);
+                this.valid = !! this.selectedCoupon;
+//                this.valid = this.coupons.map(coupon => coupon.code).includes(this.code);
+
+                if (this.valid) {
+                    this.$emit('applied', this.selectedCoupon.discount);
+                }
             }
         }
     }
--- test/CouponCode.spec.js
+++ test/CouponCode.spec.js
@@ -4,21 +4,70 @@ import CouponCode from '../src/components/CouponCode.vue';
 
 describe('CouponCode', () => {
 
-    it ('accepts a coupon code', () => {
-        let wrapper = mount(CouponCode);
+    let wrapper;
 
+    beforeEach(() => {
+        wrapper = mount(CouponCode);
+
+        // this is performed via an axios call in the .vue component, but here we use this as a fixture of sorts...
+        wrapper.setData({
+            coupons: [
+                {
+                    code: '50OFF',
+                    message: '50% Off!',
+                    discount: 50
+                },
+            ]
+        });
+    });
+
+    it ('accepts a coupon code', () => {
         expect(wrapper.contains('input.coupon-code')).toBe(true);
     });
 
-    it ('validates a user-provided coupon-code', () => {
-        let wrapper = mount(CouponCode);
-
-        let couponCode = wrapper.find('.coupon-code');
-
-        couponCode.element.value = '50OFF';
-        couponCode.trigger('input');
+    it ('validates a real coupon-code', () => {
+        enterCouponCode('50OFF');
 
         expect(wrapper.html()).toContain('Coupon Redeemed: 50% Off!');
     });
+
+    it ('validates a fake coupon-code', () => {
+        enterCouponCode('FAKE');
+
+        expect(wrapper.html()).toContain('Invalid Coupon Code');
+    });
+
+    it ('broadcasts the percentage discount when a valid coupon code is applied', () => {
+
+        // below are two techniques; one if you want to test via the ui and
+        // a second, if you want to just test the methods themselves...
+
+        // - you can just use one or the other; or both...
+        // - the second technique locks you to the code; you are forced to change 'code' if it
+        //   changes.
+        // - the first technique does not...
+
+        // ui technique; here we go through the ui
+        enterCouponCode('50OFF');
+
+        // method technique; here we just exercise the methods.
+        wrapper.setData({
+           code: '50OFF'
+        });
+
+        wrapper.vm.validate();
+
+
+        expect(wrapper.emitted().applied).toBeTruthy();
+        expect(wrapper.emitted().applied[0]).toEqual([50]);
+    });
+
+    function enterCouponCode(code) {
+        let couponCode = wrapper.find('.coupon-code');
+
+        couponCode.element.value = code;
+        couponCode.trigger('input');
+
+    }
 
 });
